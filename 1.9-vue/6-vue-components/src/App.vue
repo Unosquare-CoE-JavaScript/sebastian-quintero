cdc8073e623961be656d8dab8b92fa98535f1b4e
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-06 11:03:37 -0500

    chore: components

---
+++ 1.9-vue/6-vue-components/src/App.vue
@@ -0,0 +1,68 @@
+<script>
+import Greeting from "./components/Greeting.vue";
+import User from "./components/User.vue";
+import AppForm from "./components/Form.vue";
+
+export default {
+  name: "App",
+  components: {
+    Greeting,
+    User,
+    AppForm,
+  },
+  data() {
+    return {
+      age: 20,
+    };
+  },
+  methods: {
+    updateAge(increment) {
+      this.age += increment;
+    },
+    logCallback() {
+      console.log("called");
+    },
+  },
+};
+</script>
+
+<template>
+  <h3>Hey</h3>
+
+  <button @click="age++">Increment Age</button>
+
+  <!-- Allows lower case and capital case -->
+  <greeting :age="age" :callback="logCallback"></greeting>
+
+  <User :age="age" @age-change="updateAge"></User>
+
+  <app-form>
+    <label>
+      <input type="email" placeholder="email" />
+    </label>
+    <label>
+      <input type="text" placeholder="user" />
+    </label>
+    <label>
+      <input type="password" placeholder="password" />
+    </label>
+  </app-form>
+
+  <AppForm></AppForm>
+
+  <AppForm>
+    <template v-slot:help>
+      <p>Contact Form</p>
+    </template>
+
+    <label>
+      <input type="email" placeholder="email" />
+    </label>
+
+    <template v-slot:actions>
+      <button>Submit</button>
+    </template>
+  </AppForm>
+</template>
+
+<style></style>
---
+++ 1.9-vue/6-vue-components/src/components/User.vue
@@ -0,0 +1,21 @@
+<script>
+const EVENT_AGE_CHANGE = "age-change";
+
+const INCREMENT_BY = 5;
+
+export default {
+  name: "User",
+  props: ["age"],
+  emits: [EVENT_AGE_CHANGE],
+  methods: {
+    onClickAge() {
+      this.$emit(EVENT_AGE_CHANGE, INCREMENT_BY);
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="onClickAge">Update Age</button>
+  <p>The user is {{ age }} years old</p>
+</template>
