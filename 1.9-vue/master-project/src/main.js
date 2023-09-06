c2b4cb707eab484d8c5b4972ffd103f99f2e8963
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-06 16:59:22 -0500

    chore: form validation

--- 1.9-vue/master-project/src/main.js
+++ 1.9-vue/master-project/src/main.js
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import { veeValidatePlugin } from './plugins/validation'
 // import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 // app.use(router)
+app.use(veeValidatePlugin)
 
 app.mount('#app')
---
+++ 1.9-vue/master-project/src/plugins/validation.js
@@ -0,0 +1,30 @@
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
+import {
+  alpha_spaces,
+  confirmed,
+  email,
+  max,
+  max_value,
+  min,
+  min_value,
+  not_one_of,
+  required
+} from '@vee-validate/rules'
+
+export const veeValidatePlugin = {
+  install(app, options) {
+    app.component('VeeForm', VeeForm)
+    app.component('VeeField', VeeField)
+    app.component('ErrorMessage', ErrorMessage)
+
+    defineRule('required', required)
+    defineRule('min', min)
+    defineRule('max', max)
+    defineRule('alpha_spaces', alpha_spaces)
+    defineRule('email', email)
+    defineRule('min_value', min_value)
+    defineRule('max_value', max_value)
+    defineRule('confirmed', confirmed)
+    defineRule('not_one_of', not_one_of)
+  }
+}
