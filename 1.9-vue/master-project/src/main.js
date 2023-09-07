f3f66895a18ded66fbc4571991c7f97b9d69260f
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-07 16:50:40 -0500

    chore: router

--- 1.9-vue/master-project/src/main.js
+++ 1.9-vue/master-project/src/main.js
@@ -5,12 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import { veeValidatePlugin } from './plugins/validation'
-// import router from './router'
+import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-// app.use(router)
+app.use(router)
 app.use(veeValidatePlugin)
 
 app.mount('#app')
