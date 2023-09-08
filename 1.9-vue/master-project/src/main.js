19168d02a6c393c03f33e100665edf96027e222f
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-08 15:51:05 -0500

    chore: directives

--- 1.9-vue/master-project/src/main.js
+++ 1.9-vue/master-project/src/main.js
@@ -7,10 +7,14 @@ import App from './App.vue'
 import { veeValidatePlugin } from './plugins/validation'
 import router from './router'
 
+import Icon from './directives/icon'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(veeValidatePlugin)
+
+app.directive('icon', Icon)
 
 app.mount('#app')
