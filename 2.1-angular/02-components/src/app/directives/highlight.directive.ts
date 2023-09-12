38e1cc2086ac6e082d0517a75dc7b80670888a0d
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-12 16:02:58 -0500

    chore: directives

--- 2.1-angular/02-components/src/app/directives/highlight.directive.ts
+++ 2.1-angular/02-components/src/app/directives/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
 export class HighlightDirective {
+  defaultBackgroundColor = null;
 
-  constructor() { }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.defaultBackgroundColor =
+      this.element.nativeElement.style.backgroundColor;
+    this.element.nativeElement.style.backgroundColor = 'yellow';
+  }
 
+  @HostListener('mouseleave') onMouseLeave() {
+    this.element.nativeElement.style.backgroundColor =
+      this.defaultBackgroundColor;
+    this.defaultBackgroundColor = null;
+  }
+
+  constructor(private element: ElementRef) {}
 }
