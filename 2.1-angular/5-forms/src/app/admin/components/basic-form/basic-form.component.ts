4aaf9dc5e4f7fcf7df06cc7b7c0c1036f583b894
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-15 13:51:26 -0500

    angular(forms): form group

--- 2.1-angular/5-forms/src/app/admin/components/basic-form/basic-form.component.ts
+++ 2.1-angular/5-forms/src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -21,8 +21,29 @@ export class BasicFormComponent {
   categoryField = new FormControl('');
   tagField = new FormControl('');
 
-  //
+  // Checkbox & Radio
   agreeField = new FormControl(false);
   genderField = new FormControl('');
   zoneField = new FormControl('');
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl('', [Validators.email]),
+    phone: new FormControl(''),
+    color: new FormControl('#000000'),
+    date: new FormControl(''),
+    category: new FormControl(''),
+    tag: new FormControl(''),
+    agree: new FormControl(false, [Validators.requiredTrue]),
+    gender: new FormControl(''),
+    zone: new FormControl(''),
+  });
+
+  submit($event) {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      this.form.markAsTouched();
+    }
+  }
 }
