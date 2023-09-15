0ec8d8502d140d5864aa505613e987e77a9120b2
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-15 15:34:15 -0500

    angular(forms): complex validation

--- 2.1-angular/5-forms/src/app/auth/components/register/register.component.ts
+++ 2.1-angular/5-forms/src/app/auth/components/register/register.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from './../../../core/services/auth.service';
+import { MyValidators } from 'src/app/utils/validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   form: UntypedFormGroup;
 
   constructor(
@@ -21,15 +25,21 @@ export class RegisterComponent implements OnInit {
     this.buildForm();
   }
 
-  ngOnInit() {
+  get passwordField() {
+    return this.form.get('password');
   }
+
+  get confirmPasswordField() {
+    return this.form.get('confirmPassword');
+  }
+
+  ngOnInit() {}
 
   register(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const value = this.form.value;
-      this.authService.createUser(value.email, value.password)
-      .then(() => {
+      this.authService.createUser(value.email, value.password).then(() => {
         this.router.navigate(['/auth/login']);
       });
     }
@@ -38,8 +48,19 @@ export class RegisterComponent implements OnInit {
   private buildForm() {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          MyValidators.isValidPassword,
+        ],
+      ],
+      confirmPassword: ['', [Validators.required]],
     });
-  }
 
+    this.confirmPasswordField.addValidators(
+      MyValidators.match(this.passwordField)
+    );
+  }
 }
--- 2.1-angular/5-forms/src/app/utils/validators.ts
+++ 2.1-angular/5-forms/src/app/utils/validators.ts
@@ -1,14 +1,40 @@
 import { AbstractControl } from '@angular/forms';
 
 export class MyValidators {
-
   static isPriceValid(control: AbstractControl) {
     const value = control.value;
     console.log(value);
     if (value > 10000) {
-      return {price_invalid: true};
+      return { price_invalid: true };
     }
     return null;
   }
 
+  static isValidPassword(control: AbstractControl) {
+    const value = control.value;
+
+    if (
+      !value
+        .split('')
+        .some(
+          (c) =>
+            '0'.charCodeAt(0) <= c.charCodeAt(0) &&
+            c.charCodeAt(0) <= '9'.charCodeAt(0)
+        )
+    ) {
+      return { password_number: true };
+    }
+
+    return null;
+  }
+
+  static match(target: AbstractControl) {
+    return function isEqual(control: AbstractControl) {
+      if (control.value !== target.value) {
+        return { match: true };
+      }
+
+      return null;
+    };
+  }
 }
