2b167e37b5e76c5f703cb08ba5f927a3364a8bb2
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-15 11:26:33 -0500

    angular(forms): basic input

---
+++ 2.1-angular/5-forms/src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss'],
+})
+export class BasicFormComponent {
+  nameField = new FormControl();
+}
